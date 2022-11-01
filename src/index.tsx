6e87a84846a1eae2748da7686f203acc2e0064c8
EmreUygur <emre.uygur@outlook.com>
2022-11-01 11:03:02 +0300

    Use hydrate instead render

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as ReactDOM from 'react-dom/client';
+import * as ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.hydrate(<App />, document.getElementById('root'));
