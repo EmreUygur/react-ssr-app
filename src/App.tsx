2a40589d10269e8fda87bfcb092f2611d38ce0ad
EmreUygur <emre.uygur@outlook.com>
2022-11-01 13:49:37 +0300

    Transfer BrowserRouter to client initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import MainPage from './views/main';
 import AboutPage from './views/about';
@@ -8,13 +8,11 @@ class App extends React.Component<{}, {}> {
   render() {
     return (
       <>
-        <Router>
-          <h1>Hello World</h1>
-          <Routes>
-            <Route path='/' element={<MainPage />} />
-            <Route path='/about' element={<AboutPage />} />
-          </Routes>
-        </Router>
+        <h1>Hello World</h1>
+        <Routes>
+          <Route path='/' element={<MainPage />} />
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
       </>
     );
   }
