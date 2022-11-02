eb5279826e37e66c5bd831b1ee7ebc178aa648e5
EmreUygur <emre.uygur@outlook.com>
2022-11-02 19:02:23 +0300

    Generate index.html inside server

---
+++ server/htmlFile.ts
@@ -0,0 +1,19 @@
+const obtainHtml = (content: string): string => `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>React Typescript SSR App</title>
+    <script defer type="application/javascript" src="/client.js"></script>
+  </head>
+  <body>
+    <div id="root">
+        ${content}
+    </div>
+  </body>
+</html>
+`;
+
+export default obtainHtml;
--- server/middleware/renderer.tsx
+++ server/middleware/renderer.tsx
@@ -1,35 +1,22 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import { StaticRouter } from "react-router-dom/server";
+import { StaticRouter } from 'react-router-dom/server';
 import { Request, Response } from 'express';
+import obtainHtml from '../htmlFile';
 
 // import our main App component
 import App from '../../src/App';
 
-const path = require('path');
-const fs = require('fs');
-
 export default (req: Request, res: Response) => {
+  if (req.get('Sec-Fetch-Dest') !== 'document') {
+    return res.sendStatus(204);
+  }
 
-  const filePath = path.resolve(__dirname, '..', 'dist', 'index.html');
+  const html: string = ReactDOMServer.renderToString(
+    <StaticRouter location={req.originalUrl}>
+      <App />
+    </StaticRouter>
+  );
 
-  fs.readFile(filePath, 'utf8', (err: any, htmlData: string) => {
-    if (err) {
-      console.error('err', err);
-      return res.status(404).end();
-    }    
-
-    const html: string = ReactDOMServer.renderToString(
-      <StaticRouter location={req.originalUrl}>
-        <App />
-      </StaticRouter>
-    );
-
-    return res.send(
-      htmlData.replace(
-        '<div id="root"></div>',
-        `<div id="root">${html}<p>SSR WORKS</p></div>`
-      )
-    );
-  });
+  return res.send(obtainHtml(html));
 };
