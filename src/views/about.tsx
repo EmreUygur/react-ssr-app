2617db5eff2205496d00b84bd9116f78fe1e6893
EmreUygur <emre.uygur@outlook.com>
2022-11-04 23:48:17 +0300

    Update pages

--- src/views/about.tsx
+++ src/views/about.tsx
@@ -4,26 +4,7 @@ class AboutPage extends React.Component<{}, {}> {
   render(): React.ReactNode {
     return (
       <>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae
-          quo ea officiis maiores labore consequatur placeat ullam dicta
-          dignissimos ex aliquam voluptatibus, laborum ipsum odit rem enim optio
-          dolorem totam.
-        </p>
-
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. A explicabo
-          quod commodi rem dignissimos incidunt vero sint praesentium dolor
-          veniam voluptatem expedita officiis, optio similique esse nostrum
-          molestias. Officia, eius.
-        </p>
-
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis
-          dolor nam nostrum optio, laboriosam eius error repudiandae autem
-          quisquam voluptatem fugit distinctio excepturi hic illum. Accusantium
-          provident illo molestiae officia.
-        </p>
+        <p>Hello, this is about page</p>
       </>
     );
   }
--- src/views/main.tsx
+++ src/views/main.tsx
@@ -4,19 +4,7 @@ class MainPage extends React.Component<{}, {}> {
   render(): React.ReactNode {
     return (
       <>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae
-          quo ea officiis maiores labore consequatur placeat ullam dicta
-          dignissimos ex aliquam voluptatibus, laborum ipsum odit rem enim optio
-          dolorem totam.
-        </p>
-
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. A explicabo
-          quod commodi rem dignissimos incidunt vero sint praesentium dolor
-          veniam voluptatem expedita officiis, optio similique esse nostrum
-          molestias. Officia, eius.
-        </p>
+        <p>Hello, this is main page</p>
       </>
     );
   }
